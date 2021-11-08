33b5fe56424867f04f830fa6ad1bbd3cccca1257
marronnier-tech <marronnier.tech@gmail.com>
2021-11-09 01:00:01 +0900

    Add python setting #38 - update for atcoder tools

--- roles/common/files/python/atcoder/atcoder-env.sh
+++ roles/common/files/python/atcoder/atcoder-env.sh
@@ -1,9 +1,11 @@
-python3 -m venv atc
 source ./bin/activate
 
 curl -sL https://deb.nodesource.com/setup_current.x | bash -
 apt -y install nodejs
 
+pip install --upgrade pip
 pip3 install online-judge-tools
 npm install -g atcoder-cli
 pip3 install selenium
+
+npm install -g atcoder-cli
