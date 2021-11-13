5e780541acbf7e12b7035f8404f848b5030ab253
marronnier-tech <marronnier.tech@gmail.com>
2021-11-13 22:10:04 +0900

    separate brew install #65 - add check token system

--- README.md
+++ README.md
@@ -1,7 +1,13 @@
 # my-env
-## To execute
+## To use
 ### For Mac OS
 
+1. Ready
+Create Github Private token and put the one on local.
+The path is `../token/github.txt`.
+See [the official document](https://docs.github.com/ja/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token) for details.
+
+2. Run
 Execute Start command
 ```
 sh start.sh
--- start.sh
+++ start.sh
@@ -1,3 +1,32 @@
+#!/bin/sh
+
+echo "If Github Personal Access Token is not registered, stop running and register one."
+echo "Did you register some token in your account on your computer?(y/n)"
+
+is_token=noinput
+
+while [ $is_token != "y" -a $is_token != "n" ]
+do
+    if [ $is_token != "noinput" ]; then
+        echo "Try again"
+    fi
+    read is_token
+
+done
+
+if [ $is_token = "n" ]; then
+    echo "Execution is interrupted."
+    echo "Create the token and put one on ../token/github.txt"
+    echo "Refer the document opened automatically"
+    open https://docs.github.com/ja/authentication/keeping-your-account-and-data-secure/creating-a-personal-access-token
+    exit 1
+fi
+
+echo "Request is accepted!"
+echo "--------------------------------------------------------------------------------------------"
+echo "Auto Install System is now started."
 brew update
 brew bundle --file=./brew/Brewfile --verbose
+echo "The Github Personal Access Token is here."
+cat ../token/github.txt
 ansible-playbook site.yml
