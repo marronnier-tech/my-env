3da713c71f2d37a4c496543b0c59d1b9424731dc
marronnier-tech <marronnier.tech@gmail.com>
2022-12-29 17:44:36 +0900

    fix: #77 remove python files

--- roles/common/files/python/atcoder/atcoder-env.sh
+++
@@ -1,9 +0,0 @@
-source ./bin/activate
-
-curl -sL https://deb.nodesource.com/setup_current.x | bash -
-apt -y install nodejs
-
-pip install --upgrade pip
-pip3 install online-judge-tools
-npm install -g atcoder-cli
-pip3 install selenium
